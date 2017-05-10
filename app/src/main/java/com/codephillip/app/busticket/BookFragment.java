67c496d898202561ea9c54fb2f12b9e6928723e0
codephillip <codephillip@gmail.com>
2017-05-10 15:57:57 +0300

    adding recycler and spinner

--- app/src/main/java/com/codephillip/app/busticket/BookFragment.java
+++ app/src/main/java/com/codephillip/app/busticket/BookFragment.java
@@ -2,11 +2,29 @@ package com.codephillip.app.busticket;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class BookFragment extends Fragment {
+import com.codephillip.app.busticket.adapters.BookAdapter;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+public class BookFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
+    RecyclerView recyclerView;
+    Map<String, Integer> cropsMap = new Hashtable<>();
+    public BookAdapter adapter;
 
     public BookFragment() {
     }
@@ -19,6 +37,39 @@ public class BookFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_book, container, false);
+        spinner = (Spinner) rootView.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        adapter = new BookAdapter(getContext(), queryCategoryTable());
+        adapter = new BookAdapter();
+        recyclerView.setAdapter(adapter);
+
+
+        List<String> categories = new ArrayList<>();
+        categories.add("Price");
+        categories.add("Date");
+        categories.add("Company");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
         return rootView;
+    }
+
+//    private CategorytableCursor queryCategoryTable() {
+//        return new CategorytableSelection().query(getContext().getContentResolver());
+//    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getContext(), "Clicked " + item, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        //dummy
     }
 }
---
+++ app/src/main/java/com/codephillip/app/busticket/adapters/BookAdapter.java
@@ -0,0 +1,80 @@
+package com.codephillip.app.busticket.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codephillip.app.busticket.R;
+
+/**
+ * Created by codephillip on 10/05/17.
+ */
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
+    private static final String TAG = BookAdapter.class.getSimpleName();
+//    private BooktableCursor dataCursor;
+    private static Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView numberView;
+        private TextView titleView;
+
+        private ViewHolder(View v) {
+            super(v);
+//            numberView = (ImageView) v.findViewById(R.id.numberImageView);
+//            titleView = (TextView) v.findViewById(R.id.title_view);
+//            titleView.setTypeface(typeface);
+        }
+    }
+
+//    public BookAdapter(Context mContext, BooktableCursor cursor) {
+//        dataCursor = cursor;
+//        context = mContext;
+//        Utils.getInstance();
+//        colourQueue = new ColourQueue();
+//    }
+
+    public BookAdapter() {
+
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View cardview = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.book_row, parent, false);
+        return new ViewHolder(cardview);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+//        dataCursor.moveToPosition(position);
+//        try {
+//            String name = dataCursor.getName();
+//            holder.titleView.setText(name);
+//            holder.numberView.setImageDrawable(Utils.generateTextDrawable(name.substring(0, 1), colourQueue));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "onClick: ");
+//                dataCursor.moveToPosition(position);
+//                context.startActivity(new Intent(context, AllSongsActivity.class).putExtra(Utils.CATEGORY, dataCursor.getName()));
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        // any number other than zero will cause a bug
+//        return (dataCursor == null) ? 0 : dataCursor.getCount();
+        return 10;
+    }
+}
