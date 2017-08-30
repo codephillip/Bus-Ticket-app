54bc8c155498dc08143e94f18e742bfe5753f766
codephillip <codephillip@gmail.com>
2017-08-30 16:01:06 +0300

    tweaked animation lib, finally worked

--- app/src/main/java/com/codephillip/app/busticket/SplashActivity.java
+++ app/src/main/java/com/codephillip/app/busticket/SplashActivity.java
@@ -2,27 +2,36 @@ package com.codephillip.app.busticket;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.wang.avi.AVLoadingIndicatorView;
 
-import java.util.TimerTask;
-
 public class SplashActivity extends AppCompatActivity {
+
+    private AVLoadingIndicatorView animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        AVLoadingIndicatorView animation = (AVLoadingIndicatorView) findViewById(R.id.animation);
-        animation.smoothToShow();
+//        animation = (AVLoadingIndicatorView) findViewById(R.id.animation);
+//        animation.startAnimation();
 
-        runOnUiThread(new TimerTask() {
-            @Override
-            public void run() {
-
-            }
-        });
+//        new CountDownTimer(30000, 1000) {
+//
+//            public void onTick(long millisUntilFinished) {
+//                if (millisUntilFinished % 1000 == 0) {
+//                    animation.smoothToShow();
+//                } else {
+//                    animation.smoothToHide();
+//                }
+//            }
+//
+//            public void onFinish() {
+//            }
+//
+//        }.start();
 
         //todo activate setup service
 //        if (Utils.isConnectedToInternet(this)) {
@@ -47,5 +56,61 @@ public class SplashActivity extends AppCompatActivity {
 //            });
 //        }
 
+//    public void startAnimation(View view) {
+////        animation.smoothToShow();
+//        animation.show();
+//    }
+//
+//    public void stopAnimation(View view) {
+//        animation.smoothToHide();
+//    }
+
+        ballPulseAnimLoader();
+
+        ballClipRotateMultipleAnimateLoader();
+
+        lineScaleLoaderExample();
+
+        ballRotateLoaderAndroidExample();
+
+        lineSpinFadeLoaderLoader();
+
+        androidBallGridBeatLoaderBar();
+
+        androidBallSpinFadeLoaderProgressBar();
+
+        ballScaleRippleProgressLoaderExample();
+    }
+
+    void ballPulseAnimLoader() {
+        findViewById(R.id.material_design_ball_pulse_loader_progress).setVisibility(View.VISIBLE);
+    }
+
+    void ballClipRotateMultipleAnimateLoader() {
+        findViewById(R.id.material_design_ball_clip_rotate_multiple_loader).setVisibility(View.VISIBLE);
+    }
+
+    void lineScaleLoaderExample() {
+        findViewById(R.id.material_design_linear_scale_progress_loader).setVisibility(View.VISIBLE);
+    }
+
+    void ballRotateLoaderAndroidExample() {
+        findViewById(R.id.material_design_ball_rotate_loader).setVisibility(View.VISIBLE);
+    }
+
+    void lineSpinFadeLoaderLoader() {
+        findViewById(R.id.material_design_linear_spin_fade_loader).setVisibility(View.VISIBLE);
+    }
+
+    void androidBallGridBeatLoaderBar() {
+        findViewById(R.id.material_design_ball_grid_beat_loader).setVisibility(View.VISIBLE);
+    }
+
+    void androidBallSpinFadeLoaderProgressBar() {
+        findViewById(R.id.material_design_ball_spin_fade_loader).setVisibility(View.VISIBLE);
+    }
+
+    void ballScaleRippleProgressLoaderExample() {
+        findViewById(R.id.material_design_ball_scale_ripple_loader).setVisibility(View.VISIBLE);
     }
 }
