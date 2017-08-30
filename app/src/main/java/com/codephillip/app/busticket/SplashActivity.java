7066238a5984774afe122d6e93829831aea2d35f
codephillip <codephillip@gmail.com>
2017-08-30 15:14:14 +0300

    adding animation

--- app/src/main/java/com/codephillip/app/busticket/SplashActivity.java
+++ app/src/main/java/com/codephillip/app/busticket/SplashActivity.java
@@ -1,11 +1,11 @@
 package com.codephillip.app.busticket;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,6 +13,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        AVLoadingIndicatorView animation = (AVLoadingIndicatorView) findViewById(R.id.animation);
+        animation.smoothToShow();
+
+        runOnUiThread(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        });
 
         //todo activate setup service
 //        if (Utils.isConnectedToInternet(this)) {
@@ -24,17 +34,17 @@ public class SplashActivity extends AppCompatActivity {
         //or directly get credentials and move to main activity
 
 
-        final int animTime = getResources().getInteger(android.R.integer.config_mediumAnimTime);
-        final ImageView logo = (ImageView) findViewById(R.id.imageView2);
-        //todo add animation
-//        while (true) {
-            logo.animate().setDuration(animTime).alpha(1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
-//                    logo.animate().setDuration(animTime).alpha(0);
-                }
-            });
+//        final int animTime = getResources().getInteger(android.R.integer.config_mediumAnimTime);
+//        final ImageView logo = (ImageView) findViewById(R.id.imageView2);
+//        //todo add animation
+////        while (true) {
+//            logo.animate().setDuration(animTime).alpha(1).setListener(new AnimatorListenerAdapter() {
+//                @Override
+//                public void onAnimationEnd(Animator animation) {
+//                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+////                    logo.animate().setDuration(animTime).alpha(0);
+//                }
+//            });
 //        }
 
     }
